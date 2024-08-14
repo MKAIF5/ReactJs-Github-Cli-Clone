f0fdd6a1b81b1425b7f6ca75a42665c911243fa0
mohammed kaif <mkaif555123@gmail.com>
2024-08-15 01:31:18 +0500

    middle last completed

--- React-Github-Cli/src/App.jsx
+++ React-Github-Cli/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Nav from "./nav.jsx";
 import SecMain from "./secMain.jsx";
 import MiddleMain from "./middleMain.jsx";
+import MiddleLast from "./middleLast.jsx";
 function App() {
   return (
     <>
       <Nav />
       <SecMain />
       <MiddleMain />
+      <MiddleLast />
     </>
   );
 }
---
+++ React-Github-Cli/src/middleLast.jsx
@@ -0,0 +1,21 @@
+function MiddleLast() {
+  return (
+    <>
+      <section>
+        <div class="main-last">
+          <h2>Try GitHub on the command line</h2>
+          <p>
+            GitHub CLI brings GitHub to your terminal. Free and open source.
+          </p>
+          <br />
+          <button>Download for Windows</button>
+          <br />
+          <br />
+          <a href="">View installation instructions →</a>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default MiddleLast
