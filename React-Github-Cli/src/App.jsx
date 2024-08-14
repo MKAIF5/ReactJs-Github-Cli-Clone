3c4aa50ea8f2d270f76499f755a686b18921c248
mohammed kaif <mkaif555123@gmail.com>
2024-08-15 01:26:42 +0500

    middle main completed

--- React-Github-Cli/src/App.jsx
+++ React-Github-Cli/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import Nav from "./nav.jsx";
 import SecMain from "./secMain.jsx";
-
+import MiddleMain from "./middleMain.jsx";
 function App() {
   return (
     <>
       <Nav />
       <SecMain />
+      <MiddleMain />
     </>
   );
 }
---
+++ React-Github-Cli/src/middleMain.jsx
@@ -0,0 +1,52 @@
+function MiddleMain() {
+  return (
+    <>
+      <div class="main">
+        <div class="main-h2">
+          <h2>Goodbye, context switching.</h2>
+          <h2> Hello, terminal.</h2>
+        </div>
+        <br />
+        <div class="main-m">
+          <div class="main-one">
+            <h3>
+              Your entire GitHub <br /> workflow
+            </h3>
+            <p>Work with issues, pull requests, checks, releases and more.</p>
+            <a href="">View all GitHub CLI commands →</a>
+          </div>
+
+          <div class="main-two">
+            <h3>Script and customize</h3>
+            <p>
+              Call the GitHub API to script almost any action, <br /> and set a
+              custom alias for any command.
+            </p>
+            <a href="">Learn about aliases and API →</a>
+          </div>
+
+          <div class="main-three">
+            <h3>Enterprise-ready</h3>
+            <p>
+              Connect to GitHub Enterprise Server in <br /> addition to
+              GitHub.com.
+            </p>
+            <a href="">Get set up with Enterprise →</a>
+          </div>
+
+          <div class="main-four">
+            <h3>We 3 community</h3>
+            <p>
+              GitHub CLI is open source and ready for your <br /> contributions.
+            </p>
+            <a href="">Contribute to CLI → </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+MiddleMain()
+
+export default MiddleMain;
